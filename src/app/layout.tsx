80484d0cfee37b62efcda32cd0e9926e59f77f15
Hisban Shiraz <hisbanbardai@gmail.com>
2025-05-22 12:21:29 -0600

    Add base styles to body element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-sm text-neutral-900 bg-neutral-200`}
       >
         {children}
       </body>
