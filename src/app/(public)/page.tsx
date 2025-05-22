2b4d05a1f44b68640671a68ee4e1bf456253eaf8
Hisban Shiraz <hisbanbardai@gmail.com>
2025-05-22 15:05:01 -0600

    Add content and styling to home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+import image from "../../../public/petsoft-preview.png";
+import Logo from "@/components/Logo";
+
 export default function HomePage() {
-  return <main>Home page</main>;
+  return (
+    <main className="bg-green-200 min-h-screen flex items-center justify-center gap-10 xl:flex-row flex-col">
+      <Image src={image} alt="Preview of home page image" />
+
+      <div>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="max-w-[600px] text-2xl font-medium">
+          Use PetCare to easily keep track of pets under your care. Get lifetime
+          access for $299.
+        </p>
+        <div className="mt-10"></div>
+      </div>
+    </main>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-sm text-neutral-900 bg-neutral-200`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-sm text-neutral-900 bg-neutral-200 min-h-screen`}
       >
         {children}
       </body>
