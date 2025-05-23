5f6f1daafeaab31652b15e21e5ef5fb28bda4d4a
Hisban Shiraz <hisbanbardai@gmail.com>
2025-05-23 15:11:48 -0600

    Add content and styling to the dashboard page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,13 @@
+import Branding from "@/components/Branding";
+import Stats from "@/components/Stats";
+
 export default function DashboardPage() {
-  return <div>Dashboard page</div>;
+  return (
+    <main>
+      <div className="flex justify-between text-white/90 py-8 items-center">
+        <Branding />
+        <Stats />
+      </div>
+    </main>
+  );
 }
