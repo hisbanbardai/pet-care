b048b14c7f5ff02660e0e4f7e573fba7a3d65eb6
Hisban Shiraz <hisbanbardai@gmail.com>
2025-05-23 12:47:28 -0600

    Add the App Header with styles

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,3 +1,5 @@
+import AppHeader from "@/components/AppHeader";
+import BackgroundPattern from "@/components/BackgroundPattern";
 import React from "react";
 
 export default function ProtectedPagesLayout({
@@ -7,8 +9,11 @@ export default function ProtectedPagesLayout({
 }) {
   return (
     <>
-      <header>Header</header>
-      {children}
+      <BackgroundPattern />
+      <div className="max-w-[1050px] mx-auto px-4">
+        <AppHeader />
+        {children}
+      </div>
     </>
   );
 }
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const routes = [
+  {
+    label: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    label: "Account",
+    path: "/account",
+  },
+];
+
+export default function AppHeader() {
+  const pathName = usePathname();
+
+  return (
+    <header className="text-white/70 flex justify-between h-[60px] items-center border-b border-white/10 py-2">
+      <Logo />
+      <nav>
+        <ul className="flex gap-6 text-xs">
+          {routes.map((route) => (
+            <li key={route.label}>
+              <Link
+                href={route.path}
+                className={cn(
+                  "rounded-sm px-2 py-1 hover:text-white transition focus:text-white",
+                  {
+                    "bg-black/10 text-white": pathName === route.path,
+                  }
+                )}
+              >
+                {route.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
