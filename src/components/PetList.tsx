ad88363642a9c2e63df3a9bfba1ff5f01257dcb5
Hisban Shiraz <hisbanbardai@gmail.com>
2025-05-24 12:41:27 -0600

    Add styling to the pet list component

--- src/components/PetList.tsx
+++ src/components/PetList.tsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
+import placeholderImage from "../../public/pet-placeholder.png";
+
 export default function PetList() {
   return (
-    <ul className="w-full bg-white shadow-md">
-      <li>Benjamin</li>
+    <ul className="w-full bg-white border-b border-black/[0.08]">
+      <li>
+        <button className="flex w-full h-[70px] items-center cursor-pointer px-5 text-base gap-3 hover:bg-neutral-100 focus:bg-neutral-100 transition-all">
+          <Image
+            src={placeholderImage}
+            alt="pet-placeholder-image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Benjamin</p>
+        </button>
+      </li>
+
+      <li>
+        <button className="flex w-full h-[70px] items-center cursor-pointer px-5 text-base gap-3 hover:bg-neutral-100 focus:bg-neutral-100 transition-all">
+          <Image
+            src={placeholderImage}
+            alt="pet-placeholder-image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Benjamin</p>
+        </button>
+      </li>
     </ul>
   );
 }
