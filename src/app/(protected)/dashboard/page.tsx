fd2a39b097c8ba919634403a3a4356478a52bcc5
Hisban Shiraz <hisbanbardai@gmail.com>
2025-05-24 12:19:43 -0600

    Implement a grid layout on dashboard page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -13,16 +13,22 @@ export default function DashboardPage() {
         <Stats />
       </div>
 
-      <div>
-        <SearchForm />
+      <div className="grid grid-rows-[45px_1fr] grid-cols-[1fr_2fr] h-[500px] gap-4">
+        <div className="col-start-1 col-span-1 row-start-1 row-span-1">
+          <SearchForm />
+        </div>
 
-        <ContentWrapper>
-          <PetList />
-        </ContentWrapper>
+        <div className="col-start-1 col-span-1 row-start-2 row-span-1">
+          <ContentWrapper>
+            <PetList />
+          </ContentWrapper>
+        </div>
 
-        <ContentWrapper>
-          <PetDetails />
-        </ContentWrapper>
+        <div className="col-start-2 col-span-1 row-start-1 row-span-full">
+          <ContentWrapper>
+            <PetDetails />
+          </ContentWrapper>
+        </div>
       </div>
     </main>
   );
