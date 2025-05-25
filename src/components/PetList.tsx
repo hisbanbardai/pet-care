65956b438fd7fa7e742c344bba03c875cff79b4a
Hisban Shiraz <hisbanbardai@gmail.com>
2025-05-24 21:51:17 -0600

    Change the background color of the selected pet item in the pet list

--- src/components/PetList.tsx
+++ src/components/PetList.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import usePetsContext from "@/hooks/usePetsContext";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export default function PetList() {
-  const { pets, handleChangeSelectedPetId } = usePetsContext();
+  const { pets, handleChangeSelectedPetId, selectedPetId } = usePetsContext();
 
   return (
     <ul className="w-full bg-white border-b border-black/[0.08]">
@@ -12,7 +13,12 @@ export default function PetList() {
         <li key={pet.id}>
           <button
             onClick={() => handleChangeSelectedPetId(pet.id)}
-            className="flex w-full h-[70px] items-center cursor-pointer px-5 text-base gap-3 hover:bg-neutral-100 focus:bg-neutral-100 transition-all"
+            className={cn(
+              "flex w-full h-[70px] items-center cursor-pointer px-5 text-base gap-3 hover:bg-neutral-100 focus:bg-neutral-100 transition-all",
+              {
+                "bg-neutral-100": selectedPetId === pet.id,
+              }
+            )}
           >
             <Image
               src={pet.imageUrl}
