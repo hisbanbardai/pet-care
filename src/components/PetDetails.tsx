9c836865649d9dd8c942645c2da9ae494a54a0f1
Hisban Shiraz <hisbanbardai@gmail.com>
2025-05-27 11:55:38 -0600

    Refactor pet details component with by creating multiple small components

--- src/components/PetDetails.tsx
+++ src/components/PetDetails.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import usePetsContext from "@/hooks/usePetsContext";
+import { TPet } from "@/lib/types";
 import Image from "next/image";
 
 export default function PetDetails() {
@@ -8,37 +9,65 @@ export default function PetDetails() {
 
   return (
     <div className="w-full h-full bg-black/[0.03] flex flex-col">
-      <div className="flex items-center bg-white px-8 py-5 border-b border-black/[0.08]">
-        <Image
-          src={selectedPet?.imageUrl}
-          alt="Pet image"
-          width={75}
-          height={75}
-          className="w-[75px] h-[75px] rounded-full object-cover"
-        />
+      {selectedPet ? (
+        <>
+          <TopBar selectedPet={selectedPet} />
+          <OtherInfo selectedPet={selectedPet} />
+          <Notes selectedPet={selectedPet} />
+        </>
+      ) : (
+        <EmptyView />
+      )}
+    </div>
+  );
+}
 
-        <h2 className="font-semibold text-3xl ml-5">{selectedPet?.name}</h2>
+function EmptyView() {
+  return (
+    <p className="text-2xl font-medium flex justify-center items-center h-full">
+      No pet selected
+    </p>
+  );
+}
+
+function TopBar({ selectedPet }: { selectedPet: TPet }) {
+  return (
+    <div className="flex items-center bg-white px-8 py-5 border-b border-black/[0.08]">
+      <Image
+        src={selectedPet?.imageUrl}
+        alt="Pet image"
+        width={75}
+        height={75}
+        className="w-[75px] h-[75px] rounded-full object-cover"
+      />
+
+      <h2 className="font-semibold text-3xl ml-5">{selectedPet?.name}</h2>
+    </div>
+  );
+}
+
+function OtherInfo({ selectedPet }: { selectedPet: TPet }) {
+  return (
+    <div className="flex justify-around py-10 px-5 text-center">
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700">
+          Owner name
+        </h3>
+        <p className="mt-1 text-lg text-zinc-800">{selectedPet?.ownerName}</p>
       </div>
 
-      <div className="flex justify-around py-10 px-5 text-center">
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
-            Owner name
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.ownerName}</p>
-        </div>
-
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
-            Age
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.age}</p>
-        </div>
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700">Age</h3>
+        <p className="mt-1 text-lg text-zinc-800">{selectedPet?.age}</p>
       </div>
+    </div>
+  );
+}
 
-      <div className="bg-white px-7 py-5 rounded-md mb-9 mx-8 border border-black[0.08] flex-1">
-        {selectedPet?.notes}
-      </div>
+function Notes({ selectedPet }: { selectedPet: TPet }) {
+  return (
+    <div className="bg-white px-7 py-5 rounded-md mb-9 mx-8 border border-black[0.08] flex-1">
+      {selectedPet?.notes}
     </div>
   );
 }
