7fd25cc480dbf21b9b03086d763fa4cf1391a0d9
Hisban Shiraz <hisbanbardai@gmail.com>
2025-05-28 13:31:04 -0600

    Add html and styling to the account page

--- src/app/(protected)/account/page.tsx
+++ src/app/(protected)/account/page.tsx
@@ -1,3 +1,19 @@
+import ContentWrapper from "@/components/ContentWrapper";
+
 export default function AccountPage() {
-  return <div>Account page</div>;
+  return (
+    <main>
+      <div className="flex justify-between text-white/90 py-8 items-center">
+        <h1 className="font-medium text-2xl">Your Account</h1>
+      </div>
+
+      <div className="h-[500px]">
+        <ContentWrapper>
+          <div className="flex justify-center items-center h-full">
+            <p>Logged in as...</p>
+          </div>
+        </ContentWrapper>
+      </div>
+    </main>
+  );
 }
