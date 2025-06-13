8442d9f371abcb2a425b240a3f4047dd6929c494
Hisban Shiraz <hisbanbardai@gmail.com>
2025-06-13 14:57:04 -0600

    Fetch pets data from the database

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -3,6 +3,7 @@ import AppHeader from "@/components/AppHeader";
 import BackgroundPattern from "@/components/BackgroundPattern";
 import PetsContextProvider from "@/contexts/PetsContextProvider";
 import SearchContextProvider from "@/contexts/SearchContextProvider";
+import prisma from "@/lib/prisma";
 import { TPet } from "@/lib/types";
 import React from "react";
 
@@ -11,15 +12,17 @@ export default async function ProtectedPagesLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
-  );
+  // const response = await fetch(
+  //   "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+  // );
 
-  if (!response.ok) {
-    throw new Error("Could not fetch pets data");
-  }
+  // if (!response.ok) {
+  //   throw new Error("Could not fetch pets data");
+  // }
 
-  const data: TPet[] = await response.json();
+  // const data: TPet[] = await response.json();
+
+  const pets = await prisma.pet.findMany();
 
   return (
     <>
@@ -27,7 +30,7 @@ export default async function ProtectedPagesLayout({
       <div className="max-w-[1050px] mx-auto px-4 flex flex-col min-h-screen">
         <AppHeader />
         <SearchContextProvider>
-          <PetsContextProvider data={data}>{children}</PetsContextProvider>
+          <PetsContextProvider data={pets}>{children}</PetsContextProvider>
         </SearchContextProvider>
         <AppFooter />
       </div>
