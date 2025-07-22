35fc67a2ea9b19a0910fc225e61dde53944f45d1
Hisban Shiraz <hisbanbardai@gmail.com>
2025-07-22 15:35:41 -0600

    Use useTransition hook to get the pending state of the create checkout session server action

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -1,13 +1,28 @@
+"use client";
+
+import { createCheckoutSession } from "@/actions/actions";
 import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
 
 export default function PaymentPage() {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <main className="flex flex-col items-center gap-8">
       <h1 className="font-medium text-2xl text-center mb-5">
         PetCare access requires payment
       </h1>
 
-      <Button>Buy lifetimte access for $99</Button>
+      <Button
+        disabled={isPending}
+        onClick={() =>
+          startTransition(async () => {
+            await createCheckoutSession();
+          })
+        }
+      >
+        Buy lifetimte access for $99
+      </Button>
     </main>
   );
 }
